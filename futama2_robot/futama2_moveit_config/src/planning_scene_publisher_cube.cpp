c6a8435de4f1792ebcb7ec8819c061266b9e2e37
Adrian Ricardez Ortigosa <adrian.ricardezortigosa@dlr.de>
2024-07-03 13:41:59 +0200

    Setting easier points and cube diagonal

--- futama2_robot/futama2_moveit_config/src/planning_scene_publisher_cube.cpp
+++ futama2_robot/futama2_moveit_config/src/planning_scene_publisher_cube.cpp
@@ -89,8 +89,8 @@ namespace futama2_moveit_config
       base_object.dimensions = {0.5, 0.5, 1.0};
 
       geometry_msgs::msg::Pose base_object_pose;
-      base_object_pose.position.x = -1.0;
-      base_object_pose.position.y = 0.0;
+      base_object_pose.position.x = -0.5;
+      base_object_pose.position.y = -0.5;
       base_object_pose.position.z = -0.5;
 
       collision_object.primitives.push_back(base_object);
@@ -105,8 +105,8 @@ namespace futama2_moveit_config
       collision_object.meshes[0] = cube_mesh;
       collision_object.mesh_poses.resize(1);
 
-      collision_object.mesh_poses[0].position.x = -1.0;
-      collision_object.mesh_poses[0].position.y = 0.0;
+      collision_object.mesh_poses[0].position.x = -0.5;
+      collision_object.mesh_poses[0].position.y = -0.5;
       collision_object.mesh_poses[0].position.z = 0.0;
 
       collision_object.mesh_poses[0].orientation.w = 1.0;
